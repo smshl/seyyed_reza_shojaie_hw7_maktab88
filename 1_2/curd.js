8b6626e3fce04b356d42d7876081c496b6d5beea
narcissus313 <srz.shl@gmail.com>
2022-12-18 05:31:16 +0330

    some changes

---
+++ 1_2/curd.js
@@ -0,0 +1,5 @@
+import { mergeData } from "../1_1/1_1.js"
+function read() {
+    console.log(mergeData);
+}
+read();
--- 2_1/curd.js
+++
@@ -1,5 +0,0 @@
-import { mergeData } from "../1_1/1_1.js"
-// function read() {
-//     return
-// }
-console.log(mergeData);
