173025dbb9429b2bae1dc01b581ae5ad9b6cfcb0
narcissus313 <srz.shl@gmail.com>
2022-12-18 05:05:52 +0330

    closed to end

--- 1_1/1_1.js
+++ 1_1/1_1.js
@@ -1,1 +1,10 @@
-import * from ('')
+import { additionalPersonData, personData } from "./person-Data.js";
+function mergeData() {
+	return personData.map((person) =>
+		Object.assign(
+			person,
+			...additionalPersonData.filter((ad) => ad.uid === person.uid)
+		)
+	);
+}
+console.log(mergeData());
