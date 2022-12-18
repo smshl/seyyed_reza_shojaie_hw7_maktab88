fa2e69c71fe7b47fa796f5ac8237986ca8e7df65
narcissus313 <srz.shl@gmail.com>
2022-12-18 03:33:08 +0330

    1_1 started

---
+++ 1_1/person-data.js
@@ -0,0 +1,76 @@
+export let personData = [
+	{
+		uid: 1,
+		firstName: "Ali",
+		lastName: "Mahdavi",
+	},
+	{
+		uid: 2,
+		firstName: "Reza",
+		lastName: "Shahmardan",
+	},
+	{
+		uid: 3,
+		firstName: "Hassan",
+		lastName: "Qolami",
+	},
+	{
+		uid: 4,
+		firstName: "Morteza",
+		lastName: "Hamedani",
+	},
+	{
+		uid: 5,
+		firstName: "Sina",
+		lastName: "Hejazi",
+	},
+	{
+		uid: 6,
+		firstName: "Hadi",
+		lastName: "Sadri",
+	},
+];
+
+export let additionalPersonData = [
+	{
+		uid: 3,
+		position: "UI Designer",
+		city: "Biarjmand",
+	},
+	{
+		uid: 1,
+		position: "Back-End Developer",
+		city: "Taleqan",
+	},
+	{
+		uid: 2,
+		position: "Front-End Developer",
+		city: "Behbahan",
+	},
+	{
+		uid: 4,
+		position: "Devops",
+		city: "Shiraz",
+	},
+	{
+		uid: 6,
+		position: "Server Admin",
+		city: "Tehran",
+	},
+	{
+		uid: 5,
+		position: "Product Manager",
+		city: "Hamedan",
+	},
+];
+
+// userData ======>
+// [
+//   {
+//     uid: 1,
+//     firstName: 'Ali',
+//     lastName: 'Mahdavi',
+//     position: 'Back-End Develope',
+//     city: 'Taleqan',
+//   }, ...
+// ];
